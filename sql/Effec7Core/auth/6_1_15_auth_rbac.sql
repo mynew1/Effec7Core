625d82dbf7dad1298f3e93abff748de7d5d64de6
Effec7 <dnlbunda@gmail.com>
2015-01-06 02:37:28 +0100

    [DB/SQL] Auth fun rbac

---
+++ sql/Effec7Core/auth/6_1_15_auth_rbac.sql
@@ -0,0 +1,4 @@
+RENAME TABLE `rbac_account_permissions` TO `fun_account_permissions`;
+RENAME TABLE `rbac_default_permissions` TO `fun_default_permissions`;
+RENAME TABLE `rbac_linked_permissions` TO `fun_linked_permissions`;
+RENAME TABLE `rbac_permissions` TO `fun_permissions`;

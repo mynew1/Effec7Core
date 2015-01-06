81a4f1e35847c83339ef1344ab200b49a30689eb
Effec7 <dnlbunda@gmail.com>
2015-01-06 23:49:59 +0100

    [Core/ICC] Set only horde ICC

--- src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
@@ -157,7 +157,7 @@ class instance_icecrown_citadel : public InstanceMapScript
             void OnPlayerEnter(Player* player) override
             {
                 if (!TeamInInstance)
-                    TeamInInstance = player->GetTeam();
+                    TeamInInstance = HORDE;
 
                 if (GetBossState(DATA_LADY_DEATHWHISPER) == DONE && GetBossState(DATA_ICECROWN_GUNSHIP_BATTLE) != DONE)
                     SpawnGunship();

0cd56c59cc2099e07822b43a66d22b0d8232b98f
Effec7 <dnlbunda@gmail.com>
2015-01-06 02:48:06 +0100

    [Core/Spell] Fix Glyph of Seduction

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5623,7 +5623,8 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
             }
             switch (dummySpell->Id)
             {
-                // Glyph of Polymorph
+                // Glyph of Polymorph + Glyph of Seduction
+                case 56250:
                 case 56375:
                 {
                     if (!target)
